4298daffc9fcd7fd1128457a1ad5dc9eb5d2fd89
subzerocr <subzerocr@users.noreply.github.com>
2015-06-06 21:34:01 -0400

    Make a bsic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606020116) do
+ActiveRecord::Schema.define(version: 20150607011721) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "last_name"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
