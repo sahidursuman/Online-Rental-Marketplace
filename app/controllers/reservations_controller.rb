6a42d369997927f658af105a3598dc38cfbf3cf3
subzerocr <subzerocr@users.noreply.github.com>
2015-07-20 22:06:23 -0400

    Reservation page half finished, working on Stripe next

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,18 @@
 class ReservationsController < ApplicationController
+require 'date' 
 
 	def show
-		@user = User.find(session[:user_id])
+		d1 = params[:datetimepicker1]
+		d2 = params[:datetimepicker2]
+		@item = Item.find_by_id(params[:item_id])
+		@lending_user = User.find(session[:user_id])
+		@lender_user = User.find_by_id(@item.user_id)
+		@subtotal = (((d2.to_datetime - d1.to_datetime)*24).to_i * (@item.lending_price/24)).ceil
+		@fee = @subtotal * 0.09
+		@total = @subtotal + @fee
+		@borrow_date = d1[5,5] + "/" + d1[0,4]
+		@due_date = d2[5,5] + "/" + d2[0,4]
+		@borrow_time = d1[11,13]
+		@due_time = d2[11,13]
 	end
 end
