9171300915b7ca3dd512a8a803f313e057c17484
subzerocr <subzerocr@users.noreply.github.com>
2015-07-26 16:25:22 -0400

    Attempt 2: fix db:migrate

--- db/migrate/20150722221236_add_unique_to_reservations.rb
+++ db/migrate/20150722221236_add_unique_to_reservations.rb
@@ -1,6 +1,5 @@
 class AddUniqueToReservations < ActiveRecord::Migration
   def change
-  	remove_index :reservations, [:item_id, :lent_id]
   	add_index :reservations, [:item_id, :lender_id, :lent_id], unique: true
   end
 end
