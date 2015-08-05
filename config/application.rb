dfa71e98b963a4509996e7de4131516114865de6
subzerocr <subzerocr@users.noreply.github.com>
2015-08-05 00:11:57 -0400

    Testing time zone local

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Armrack
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/app/uploaders)
     config.action_view.embed_authenticity_token_in_remote_forms = true
